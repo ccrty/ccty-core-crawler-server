c6fbf77e839fb914670f4e1d1b3860ddf3d3738b
chentao <18217613265@163.com>
2020-05-27 16:39:03 +0800

    加入httpclient工具类

---
+++ src/main/java/com/jianmo/crawler/util/http/HTTPSClientTest.java
@@ -0,0 +1,24 @@
+package com.jianmo.crawler.util.http;
+
+
+
+import org.apache.http.client.HttpClient;
+
+import java.util.HashMap;
+
+public class HTTPSClientTest {
+
+    public static void main(String[] args) throws Exception {
+        HttpClient httpClient = null;
+
+        httpClient = new HTTPSTrustClient().init();
+
+        String url = "https://igc-es.wucb.com/ibm/iis/dq/da/rest/v1/workspaces/overview";
+
+
+        String result = HTTPSClientUtil.doGet(httpClient, url, new HashMap<>(),new HashMap<>());
+
+        System.out.println(result);
+    }
+
+}
